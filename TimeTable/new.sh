474f3dd330810d4c7b625998de5842544a537e84
vgswn <iit2015038@iiita.ac.in>
2017-09-14 09:25:06 +0530

    changes

--- TimeTable/new.sh
+++ TimeTable/new.sh
@@ -1,1 +1,1 @@
-./a.out /home/vips/Documents/timetable_generator_5thSem_project-master/timetable_generator_5thSem_project-master/TimeTable/inp.txt
+./a.out /home/vips/timetable_generator_5thSem_project/TimeTable/inp.txt

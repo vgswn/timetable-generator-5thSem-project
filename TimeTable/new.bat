d3e75503511d0abd27f4ce2795662b8bd5135435
vgswn <iit2015038@iiita.ac.in>
2017-11-16 23:37:03 +0530

    post_mid_sems

--- TimeTable/new.bat
+++
@@ -1,2 +0,0 @@
-new.exe C:\Users\Rakalicious\Downloads\timetable_generator_5thSem_project-master\timetable_generator_5thSem_project-master\TimeTable\inp.txt
-exit
--- TimeTable/new.sh
+++
@@ -1,1 +0,0 @@
-./a.out /home/vips/Desktop/timetable_generator_5thSem_project/TimeTable/inp.txt
